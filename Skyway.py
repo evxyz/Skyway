129b7c745d3dcb528aacd5750d214e713ceb02f9
evx001 <eviexeris@yahoo.com>
2012-04-18 06:21:31 -0400

    2012-0418-0600

---
+++ Skyway.py
@@ -0,0 +1,283 @@
+        SKYWAY
+
+										-I-
+
+ 
+
+The girl
+
+(who would become known to herself as “the missing girl”)
+
+sleeps,
+
+    head slumped against the passenger window of the blue Honda
+
+           rushing along the I-5 Skyway.
+
+ 
+
+In the girl's dream:
+she is in the bedroom 
+of a composite house pulled together from bits of others they've already lived in; 
+the girl grabs the angel-figurine from the nativity-set on the bedroom bureau just before a disembodied arm sweeps the entire set into a garbage bag.
+
+“We don't have time, we've got to go. Now."
+
+says a disembodied mother like voice.
+
+										* * * * * * *
+
+The blue Honda hits the railing
+
+and the girl is jolted up through dream layers absently touching at the mark on her forehead,
+
+like when she rode the Whirlygig ride at the Johnson County fair, last summer, 
+the summer before they'd had to move again, 
+in the middle of the night, always . . . 
+the middle of the night.
+
+ 
+
+The blue Honda launches out of the southbound lane of the I-5 Skyway
+
+        into open space above treetops.
+
+ 
+
+												-II-
+
+ 
+
+Hours later, against a blue dawn sky, right at the gap in the railing where the car went over; State-Trooper Marlowe will look down into a clump of shattered pine trees and mashed blue metal, that used to be a Honda Civic, (identifiable, only by the name-plate on the trunk) and fill out his accident report.
+
+ 
+
+												-III-
+
+ 
+
+Earth and sky change places for the girl in the blue Honda,
+
+in a tumble,      
+
+like laundry in a cold dryer
+
+               
+
+Air,
+rushes in,
+across her cheek,
+Air,
+from an open door.
+Air
+from a missing door. 
+“Mom?”
+
+											-IV-
+
+“Thrown” seven yards in a “south easterly direction” from the “crash site” State Trooper Marlowe's report will say in triplicate.
+
+											-V-
+
+But the girl
+
+isn't thinking of reports,
+
+or last summer,
+
+or amusement-park rides.
+
+ 
+
+She’s touching at the mark, a dent, a divit, 
+(her central defect)
+on her forehead;
+
+It’s like she’s crossing herself
+but she isn’t crossing herself,
+she’s flailing
+
+screaming
+or dreams screaming,
+screaming in her mind,
+screaming in her heart,
+but hearts can't scream, 
+						can they?
+
+. . . and down the blue Honda Civic rolls, & twists, through pine branches, disgorging its driver, enfolding its passenger,
+
+as it impacts the night
+						-earth.
+
+											-VI-
+
+3am 
+
+the median 
+
+under the Route 5 Skyway in the Crown-Vic’ patrol-car Trooper Marlowe,
+
+his face illuminated, blue by dashboard light.
+
+Night air and night sounds enter through the open car window.
+
+Outside, wind draws up angry, 
+								sudden.
+
+Sound follows the wind;
+
+the sound of metal,
+
+ri
+pp
+ing,
+
+tear ing,
+
+ 
+
+An engine races to scream.
+
+tree-wood shatters, 
+
+as if struck by a canon.
+
+ 
+
+A something hits the tree row below the I-5 Skyway.
+
+Trooper Marlow leaves his spot on the median
+
+          police-lights on, siren on.
+
+Heart racing,
+
+          Marlow approaches, slow.
+
+Fearful of running over the injured?
+
+He must push through his 3am fog bank.
+
+On the North side of 390,
+
+          where it crosses under the Skyway,
+
+a pine tree: 
+sheared in two 
+				by the “probable” remains 
+of a vehicle 
+			 pulled apart 
+			 into toffee-like strands.
+
+Seconds balloons to minutes, he moves as if underwater
+
+unable to make his limbs move faster striding up the embankment to the disemboweled wreckage.
+
+In the stillness: 
+				the ticking steam of antifreeze on scalding exhaust.
+
+ What can’t be in Trooper Marlowe’s report 
+(but should be): 
+	the time taken for him to find “the mother,” 
+	in tall grass.
+		Convulsing, 
+		limbs expanding, 
+		mouth, 
+			blood-frothing.
+
+The top of the woman's head, (inches above the eye line) sheared off.
+
+He looks down into her sightless eyes; 
+he tilts his head trying to imagine the sequence of events that puts her here.
+  
+Over his shoulder, 
+a girl, 
+(or the “sense” of a girl) 
+					floating 
+							above the accident 
+looking down 
+			at him, 
+			but when he 
+			looks up he can’t see anything
+just a night full of winter stars.
+
+
+Compelled, he turns away from the woman and goes back to the wreckage.
+
+To look around,
+				To look underneath,
+										To look within.
+Beneath the car-roof curled like a blanket,
+engulfing 
+			the only remaining occupant.
+Visible? 
+			A blood soaked knit cap with raccoon ears.
+
+A hand emerges from under metal, 
+a girl's hand, 
+small 
+open 
+	extending toward him.
+
+He touches the palm;
+
+					feels the warmth.
+The small hand closes tight around his fingers.
+
+ 
+
+									-VII-
+
+									
+vapor 					 									conscious  	
+outside         				the             			outside 
+crushing     		weight     			of            no,weight
+light           of               infinite                            dark
+        bird
+                                                                    wing,free
+the 			float     					space
+
+in
+
+space  
+
+                                      within  
+
+                                                                                   space
+
+						  place       
+												&         	
+																	no,place
+
+                                    time              no,time
+
+where  
+		no-------------------------------------------------------------------------------where
+
+                    
+                                             
+                                            -VIII-
+
+                                            
+the girl on the edge of America: 
+held in angel arms, warm as the wind in her face; she is moving toward the light surrounded by darkness;
+
+ 
+
+the girl on the edge of America: 
+sees herself little and dancing, on the pier at  Cayuga Lake, only to fall in the water, laughing;
+
+ 
+
+the girl on the edge of America: 
+sees a blank faced man who is either her father or a man who will be;
+
+ 
+
+the girl on the edge of America: 
+is about to slip out of America, float-away, forever or maybe 
+																come back.
+
+ 
+
+ 
