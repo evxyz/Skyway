30ef40f2a6e932996ebf89b5a9d7e3a6eac54d5c
evx001 <eviexeris@gmail.com>
2015-12-30 17:23:30 -0500

    notes added

---
+++ notes/Skyway.py
@@ -0,0 +1,285 @@
+        SKYWAY
+
+							-I-
+
+ 
+
+
+The girl on the edge of America: 
+
+	(who would become known to herself as "the missing girl")
+
+	sleeps,
+
+    	head slumped against the passenger window of the blue Honda
+
+           rushing along the I-5 Skyway.
+
+ 
+
+In the girl's dream:
+she is in the bedroom 
+of a composite house pulled together from bits of others they've already lived in; 
+the girl grabs the angel-figurine from the nativity-set on the bedroom bureau just before a disembodied arm sweeps the entire set into a garbage bag.
+
+"We don't have time, we've got to go. Now."
+
+says a disembodied mother like voice.
+
+						* * * * * * *
+
+The blue Honda hits the railing
+
+and the girl is jolted up through dream layers absently touching at the mark on her forehead,
+
+like when she rode the Whirlygig ride at the Johnson County fair, last summer, 
+the summer before they'd had to move again, 
+in the middle of the night, always . . . 
+the middle of the night.
+
+ 
+
+The blue Honda launches out of the southbound lane of the I-5 Skyway
+
+        into open space above treetops.
+
+ 							-II-
+
+ 
+
+Hours later, against a blue dawn sky, right at the gap in the railing where the car went over; State-Trooper Marlowe will look down into a clump of shattered pine trees and mashed blue metal, that used to be a Honda Civic, (identifiable, only by the name-plate on the trunk) and fill out his accident report.
+
+ 
+
+							-III-
+
+ 
+
+Earth and sky change places for the girl in the blue Honda,
+
+in a tumble,      
+
+like laundry in a cold dryer
+
+               
+
+Air,
+rushes in,
+across her cheek,
+Air,
+from an open door.
+Air
+from a missing door. 
+"Mom?"
+
+							-IV-
+
+
+"Thrown" seven yards in a "south easterly direction" from the "crash site" State Trooper Marlowe's report will say in triplicate.
+
+							-V-
+
+But the girl
+
+isn't thinking of reports,
+
+or last summer,
+
+or amusement-park rides.
+
+ 
+
+She's touching at the mark, a dent, a divit, 
+(her central defect)
+on her forehead;
+
+It's like she's crossing herself
+but she isn't crossing herself,
+she's flailing
+
+screaming
+or dreams screaming,
+screaming in her mind,
+screaming in her heart,
+but hearts can't scream, 
+						can they?
+
+. . . and down the blue Honda Civic rolls, & twists, through pine branches, disgorging its driver, enfolding its passenger,
+
+as it impacts the night
+						-earth.
+
+							-VI-
+
+3am 
+
+the median 
+
+under the Route 5 Skyway in the Ford Crown-Vic', trooper Marlowe,
+
+his face illuminated, blue by dashboard light.
+
+Night air and night sounds enter through the open car window.
+
+Outside, wind draws up angry, 
+								sudden.
+
+Sound follows the wind;
+
+the sound of metal,
+
+ri
+pp
+ing,
+
+tear ing,
+
+ 
+
+An engine races to scream.
+
+tree-wood shatters, 
+
+as if struck by a canon.
+
+ 
+
+A something hits the tree row below the I-5 Skyway.
+
+Trooper Marlow leaves his spot on the median
+
+          police-lights on, siren on.
+
+Heart racing,
+
+          Marlow approaches, slow.
+
+Fearful of running over the injured?
+
+He must push through his 3am fog bank.
+
+On the North side of 390,
+
+          where it crosses under the Skyway,
+
+a pine tree: 
+sheared in two 
+				by the "probable" remains 
+of a vehicle 
+			 pulled apart 
+			 into toffee-like strands.
+
+Seconds balloon to minutes, he moves as if underwater
+
+unable to make his limbs move faster striding up the embankment to the disemboweled wreckage.
+
+In the stillness: 
+				the ticking steam of antifreeze on scalding exhaust.
+
+ What can't be in Trooper Marlowe's report 
+(but should be): 
+	the time taken for him to find "the mother," 
+	in tall grass.
+		Convulsing, 
+		limbs expanding, 
+		mouth, 
+			blood-frothing.
+
+The top of the woman's head, (inches above the eye line) sheared off.
+
+He looks down into her sightless eyes; 
+he tilts his head trying to imagine the sequence of events that puts her here.
+  
+Over his shoulder, 
+a girl, 
+(or the "sense" of a girl) 
+					floating 
+							above the accident 
+looking down 
+			at him, 
+			but when he 
+			looks up he can't see anything
+just a night full of winter stars.
+
+
+Compelled, he turns away from the woman and goes back to the wreckage.
+
+To look around,
+				To look underneath,
+										To look within.
+Beneath the car-roof curled like a blanket,
+engulfing 
+			the only remaining occupant.
+Visible? 
+			A blood soaked knit cap with raccoon ears.
+
+A hand emerges from under metal, 
+a girl's hand, 
+small 
+open 
+	extending toward him.
+
+He touches the palm;
+
+					feels the warmth.
+The small hand closes tight around his fingers.
+
+ 
+
+							-VII-
+
+									
+
+a vapor 
+outside the outside 
+the liberating weight of no weight 
+a flicker of light above infinite dark
+a bird, wingless
+but afloat      					  
+no-------------------------------------------------------------------------------where
+
+                    
+                                             
+        	                                    -VIII-
+	
+
+“the girl” f
+                   a
+                       l 
+                         l
+                          s awake, 
+	                          awake to tubes: 
+		                          nose tubes, 
+			                          mouth tubes, 
+				                          stuck in the arm tubes.
+
+Tubes like so many roads leading into and going out of her. 
+She is a city of tubes, and beeping sounds, whirring sounds, mechanical breathing sounds. 
+
+an elephant of weight on her legs and arms.
+
+A word tries to rise in her throat but it's caught in the tube. 
+She jerks her middle and 
+	plastic pipes running into her, tap against their metal parts, a dull tap like a clocks ticking. 
+
+An itch forms on her forehead  
+but she can't reach to wipe, 
+her head bandaged, taped, 
+taped together, 
+if she moves to hard her whole head will come apart.
+
+Then sleep or night 
+washes over it's banks and sweeps her back down into black.
+
+White day voices, 
+the blur of hushed talking 
+
+“the girl,” 
+	which must be her. 
+Sometimes she's "the girl in seven" but mostly it is just "the girl." 
+
+an occasional sentence becomes discernible / meaningful
+"someone will claim her?"
+
+But no one comes, 
+	only nurses.  
